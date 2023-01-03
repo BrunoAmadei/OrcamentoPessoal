0f74fd5f73ca25c4fe36223218551ece15782d66
BrunoAmadei <brunoamadei98@gmail.com>
2023-01-03 09:23:51 -0300

    Update orcamento.js

--- src/js/orcamento.js
+++ src/js/orcamento.js
@@ -3,5 +3,8 @@ function cadastrarDespesa() {
     document.getElementById('ano')
     document.getElementById('mes')
     document.getElementById('dia')
+    document.getElementById('tipo')
+    document.getElementById('descricao')
+    document.getElementById('valor')
 
 }
