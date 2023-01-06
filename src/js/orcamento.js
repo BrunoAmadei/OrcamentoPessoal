3b384c9ee03cc5f7ac2df9ef2580d6745d74f2d2
BrunoAmadei <brunoamadei98@gmail.com>
2023-01-06 10:24:58 -0300

    Update orcamento.js

--- src/js/orcamento.js
+++ src/js/orcamento.js
@@ -94,6 +94,7 @@ class Bd {
         if (despesa.valor != '') {
             despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor)
         }
+        return despesasFiltradas
     }
 }
 const bd = new Bd()
@@ -147,51 +148,57 @@ function cadastrarDespesa() {
 }
 
 // Carregando despesas
-function listaDespesas() {
+function listaDespesas(despesas = Array(), filtro = false) {
 
-    let despesas = Array()
+    if (despesas.length == 0 && filtro == false) {
+        despesas = bd.carregarRegistro()
+    }
 
-    despesas = bd.carregarRegistro()
+    let listaDespesas = document.getElementById("listaDespesas")
+    listaDespesas.innerHTML = ''
+    despesas.forEach(function (d) {
 
-    const listaDespesas = document.getElementById('listaDespesas')
+        //Criando a linha (tr)
+        const linha = listaDespesas.insertRow();
 
-    //percorrendo array e listando dinamicamente
-    despesas.forEach(d => {
-
-        // criando linhas da tabela
-        const linha = listaDespesas.insertRow()
-
-        // criando colunas da tabela
+        //Criando as colunas (td)
         linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
 
-        // ajustando tipo
-        switch (parseInt(d.tipo)) {
-            case 1: d.tipo = 'Alimentação'
+        //Ajustar o tipo
+        switch (d.tipo) {
+            case '1': d.tipo = 'Alimentação'
                 break
-            case 2: d.tipo = 'Educação'
+            case '2': d.tipo = 'Educação'
                 break
-            case 3: d.tipo = 'Lazer'
+            case '3': d.tipo = 'Lazer'
                 break
-            case 4: d.tipo = 'Saúde'
+            case '4': d.tipo = 'Saúde'
                 break
-            case 5: d.tipo = 'Transporte'
+            case '5': d.tipo = 'Transporte'
                 break
+
         }
         linha.insertCell(1).innerHTML = d.tipo
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = d.valor
     })
+
 }
 
+
 function pesquisarDespesa() {
-    const ano = document.getElementById('ano').value
-    const mes = document.getElementById('mes').value
-    const dia = document.getElementById('dia').value
-    const tipo = document.getElementById('tipo').value
-    const descricao = document.getElementById('descricao').value
-    const valor = document.getElementById('valor').value
 
-    const despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+    let ano = document.getElementById("ano").value
+    let mes = document.getElementById("mes").value
+    let dia = document.getElementById("dia").value
+    let tipo = document.getElementById("tipo").value
+    let descricao = document.getElementById("descricao").value
+    let valor = document.getElementById("valor").value
 
-    bd.pesquisar(despesa)
+    let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+
+    let despesas = bd.pesquisar(despesa)
+
+    this.listaDespesas(despesas, true)
+
 }
